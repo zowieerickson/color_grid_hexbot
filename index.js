b02f3d8860a73f849af9ead2bf4f241a9866ab1e
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-26 18:34:23 -0400

    display colors in a grid using flexbox, just practicing flexbox with fetch

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 async function getColors() {
-    const response = await fetch(`https://apis.scrimba.com/hexcolors?count=15`);
+    const response = await fetch(`https://apis.scrimba.com/hexcolors?count=250`);
     const data = await response.json();
 
     const colors = data.colors
 
-    console.log(colors);
+    displayColors(colors)
 }
 
-function displayColors() {
-    const html = ``;
-    
+function displayColors(colorArr) {
+    console.log(colorArr)
+    let html = '';
+
+    const stuff = colorArr.map(color => {
+        // console.log(color)
+        html = `<div class="color" style="background-color: ${color.value}">${color.value}</div>`;
+        return html;
+    })
+
+    for (let i = 0; i < stuff.length; i++) {
+        html += stuff[i];
+        console.log(html)
+    }
+    document.body.innerHTML = html
 }
 
 getColors()
