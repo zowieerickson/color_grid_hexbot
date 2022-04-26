6b06a482f370220187a217e89ada9631ede8890d
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-26 17:58:12 -0400

    fetching colors from hexColor api

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+async function getColors() {
+    const response = await fetch(`https://apis.scrimba.com/hexcolors?count=15`);
+    const data = await response.json();
+
+    const colors = data.colors
+
+    console.log(colors);
+}
+
+function displayColors() {
+    const html = ``;
+    
+}
+
+getColors()
