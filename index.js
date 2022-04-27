ddd41a58d1266986d60bd37232cbba519ba4fdb6
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-27 10:29:13 -0400

    color blocks now checker across screen

--- index.js
+++ index.js
@@ -8,18 +8,15 @@ async function getColors() {
 }
 
 function displayColors(colorArr) {
-    console.log(colorArr)
     let html = '';
 
     const stuff = colorArr.map(color => {
-        // console.log(color)
-        html = `<div class="color" style="background-color: ${color.value}">${color.value}</div>`;
+        html = `<div class="color" style="background-color: ${color.value}"><p>${color.value}</p></div>`;
         return html;
     })
 
     for (let i = 0; i < stuff.length; i++) {
         html += stuff[i];
-        console.log(html)
     }
     document.body.innerHTML = html
 }
